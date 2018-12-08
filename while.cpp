1ed4b3b5dbf65c5e178003a0a60bef71cf08b6eb
IKHSAN MAULANA <45529723+ikhsanmaulana15@users.noreply.github.com>
2018-12-08 20:35:56 +0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+ 
+   int a = 1;
+
+   
+   while( a < 10 ) {
+      cout << "value of a: " << a << endl;
+      a++;
+   }
+
+   return 0;
+}
